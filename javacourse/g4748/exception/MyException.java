72a0bf5b3c9d5188f1cc57d9e206b96af30a42be
Sergii Kozhushko <s0504104302@gmail.com>
2022-11-28 16:53:29 +0100

    Test push message

---
+++ javacourse/g4748/exception/MyException.java
@@ -0,0 +1,12 @@
+/*
+   Programmed by Sergii Kozhushko 2022
+   Date of creation: 28-Nov-2022 
+*/
+
+package edu.javacourse.g4748.exception;/*
+Programmed by Sergii Kozhushko
+*/
+
+public class MyException {
+
+}
