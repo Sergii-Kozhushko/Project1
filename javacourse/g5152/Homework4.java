23e59372aa993b41b251e0372aefde6e4c1335bf
Sergii Kozhushko <s0504104302@gmail.com>
2022-12-14 15:45:47 +0100

    Домашка по классу

---
+++ javacourse/g5152/Homework4.java
@@ -0,0 +1,12 @@
+/**
+ * Homework4.java
+ *
+ * @author Sergii Kozhushko, sergiikozhushko@gmail.com
+ * Date of creation: 14-Dec-2022 15:44
+ */
+
+package edu.javacourse.g5152;
+
+public class Homework4 {
+
+}
